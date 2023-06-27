08dc1b8d79688405b5f338e477918d510608af98
Marcela Pino <marcelampino2018@gmail.com>
2023-06-27 17:06:16 -0400

    First commit of mygreenfield react project.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Confetti from "react-confetti";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Confetti />
+      <h1 class="first">Wow!</h1>
+      <h1 class="second"> I'm working with React on my machine!</h1>
+      <p class="third">Is this what happiness feels like?</p>
+      
+    </div>
+  );
+}
+
+export default App;
